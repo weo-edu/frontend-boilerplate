15740d039e8e70e01c2a4c443865b02a2e6e33d3
Andrew Shaffer <andrew@weo.io>
2013-11-27 17:48:26 -0800

    initial commit

---
+++ client.js
@@ -0,0 +1,9 @@
+var angula = require('angular');
+var jquery = require('jquery');
+var test = 'tddessdft';
+var component = require('./build');
+var moment = component('component-moment');
+var boot = component('LearnBoost-jsonp');
+console.log('angular', angular);
+console.log('jquery', jquery);
+console.log('moment', moment);
